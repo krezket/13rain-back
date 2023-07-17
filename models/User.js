1013102b5819772cdffafd9e8e202062c2511203
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-07-16 18:08:43 -0700

    updates

--- models/User.js
+++ models/User.js
@@ -12,6 +12,7 @@ User.init(
     {
         id: {
             type: DataTypes.INTEGER,
+            // defaultValue: DataTypes.UUIDV4,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
