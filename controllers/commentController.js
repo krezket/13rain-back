4f1ceb23b9b6e709cce1cd900a3143ce2f2f9ce8
Anthony Work PC <anthonyguerrero62@outlook.com>
2024-03-08 11:22:40 -0800

    removed ids from controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,18 +2,7 @@ const router = require('express').Router();
 const { Page, User, Comments } = require('../models');
 
 router.get('/', (req, res) => {
-    Comments.findAll({
-        include: [
-            {
-                model: User,
-                as: 'users',
-            },
-            {
-                model: Page,
-                as: 'pages',
-            }
-        ]
-    })
+    Comments.findAll()
     .then(pages => {
         res.json(pages)
     }).catch(err => {
