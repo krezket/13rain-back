26b15840841ce2a097b82aff0e0a069d4058fa36
Anthony Work PC <anthonyguerrero62@outlook.com>
2024-03-08 08:23:57 -0800

    created comment model

---
+++ models/Comments.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {};
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        page_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'page',
+                key: 'id',
+            },
+        },
+    },{
+        sequelize
+    }
+);
