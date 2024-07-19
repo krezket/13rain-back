d43cf22124d0cc1657fe030d688f0bfa2f87abc4
Anthony <anthonyguerrero62@outlook.com>
2024-07-19 09:21:44 -0700

    saved

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,172 +4,18 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 // Get all users
-router.get('/', (req, res) => {
-    User.findAll({
-        include: [
-            {
-                model: Page,
-                as: 'pages',
-            },
-            {
-                model: Comments,
-                as: 'comments',
-            },
-            {
-                model: User,
-                as: 'followers',
-                attributes: { exclude: ['password', 'bio'] }
-            },
-            {
-                model: User,
-                as: 'following',
-                attributes: { exclude: ['password', 'bio'] }
-            }
-        ]
-    })
-    .then(users => {
-        res.json(users)
-    }).catch(err => {
-        res.status(500).json({
-            msg:"error", err
-        });
-    });
-});
-
-// Get user by ID
-router.get("/:id", (req, res) => {
-    User.findByPk(req.params.id, {
-        include: [
-            {
-                model: Page,
-                as: 'pages',
-            },
-            {
-                model: User,
-                as: 'followers',
-                attributes: { exclude: ['password', 'bio'] }
-            },
-            {
-                model: User,
-                as: 'following',
-                attributes: { exclude: ['password', 'bio'] }
-            }
-        ],
-    })
-    .then(userData => {
-        if(!userData) {
-            return res.status(404).json({msg: "no such user"})
-        }
-
-        res.json(userData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({msg:"womp womp", err})
-    });
-});
-
-// GET PROFILE BY USERNAME
-router.get("/profile/:username", (req, res) => {
-    User.findOne({where: {username: req.params.username},
-        include: [
-            {
-                model: Page,
-                as: 'pages',
-            },
-            {
-                model: User,
-                as: 'followers',
-                attributes: {exclude: ['password', 'bio']}
-            },
-            {
-                model: User,
-                as: 'following',
-                attributes: {exclude: ['password', 'bio']}
-            }
-        ],
-    })
-    .then(userData => {
-        if(!userData) {
-            return res.status(404).json({msg: "bruh"})
-        }
-
-        res.json(userData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({msg:"womp womp", err})
-    });
-});
-
-// Create a user
-router.post('/', (req, res) => {
-    console.log(req.body)
-    User.create(req.body)
-    .then(newUser => {
-        const token = jwt.sign({
-            username: newUser.username,
-            userId: newUser.id
-        },process.env.JWT_SECRET,{
-            expiresIn:"2h"
-        })
-        res.json({
-            token,
-            user: newUser
-        })
-    }).catch(err => {
-        console.log(err)
-        res.status(500).json({
-            msg:"error creating user", 
-            err
-        });
-    });
-});
-
-// Login
-router.post('/login', (req, res) => {
-    console.log(req.body)
-    User.findOne({where: {username: req.body.username}})
-    .then(userData => {
-        if(!userData) {
-            console.log("USERNAME NOT FOUND")
-            res.status(403).json({ msg: "invalid login" });
-        } else if (!bcrypt.compareSync(req.body.password, userData.password)) {
-            return res.status(401).json({msg:"invalid login"})
-        } else {
-            const token = jwt.sign({
-                username:userData.username,
-                userId:userData.id
-            },process.env.JWT_SECRET,{
-                expiresIn:"2h"
-            })
-            res.json({
-                token,
-                user:userData
-            })
-        }
-    }).catch(err => {
-        console.log(err)
-        res.status(500).json({
-            msg:"error logging in", 
-            err
-        });
-    });
-});
-
-// Verify Token
-router.get("/auth/verifytoken",(req,res)=>{
-    const token = req.headers.authorization?.split(" ")[1];
+router.get('/', async (req, res) => {
     try {
-        const data = jwt.verify(token,process.env.JWT_SECRET)
-        User.findByPk(data.userId, {
+        const users = await User.findAll({
             include: [
-                // {
-                //     model: Page,
-                //     as: 'pages',
-                // },
-                // {
-                //     model: Comments,
-                //     as: 'comments',
-                // },
+                {
+                    model: Page,
+                    as: 'pages',
+                },
+                {
+                    model: Comments,
+                    as: 'comments',
+                },
                 {
                     model: User,
                     as: 'followers',
@@ -181,34 +27,190 @@ router.get("/auth/verifytoken",(req,res)=>{
                     attributes: { exclude: ['password', 'bio'] }
                 }
             ]
-        })
-        .then(foundUser=>{
-            res.json(foundUser)
-        })
+        });
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({
+            msg: "error",
+            err
+        });
+    }
+});
+
+// Get user by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Page,
+                    as: 'pages',
+                },
+                {
+                    model: User,
+                    as: 'followers',
+                    attributes: { exclude: ['password', 'bio'] }
+                },
+                {
+                    model: User,
+                    as: 'following',
+                    attributes: { exclude: ['password', 'bio'] }
+                }
+            ],
+        });
+
+        if (!userData) {
+            return res.status(404).json({ msg: "no such user" });
+        }
+
+        res.json(userData);
     } catch (err) {
         console.log(err);
-        res.status(403).json({msg:"bad token",err})
+        res.status(500).json({ msg: "womp womp", err });
+    }
+});
+
+// GET PROFILE BY USERNAME
+router.get("/profile/:username", async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: { username: req.params.username },
+            include: [
+                {
+                    model: Page,
+                    as: 'pages',
+                },
+                {
+                    model: User,
+                    as: 'followers',
+                    attributes: { exclude: ['password', 'bio'] }
+                },
+                {
+                    model: User,
+                    as: 'following',
+                    attributes: { exclude: ['password', 'bio'] }
+                }
+            ],
+        });
+
+        if (!userData) {
+            return res.status(404).json({ msg: "bruh" });
+        }
+
+        res.json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "womp womp", err });
+    }
+});
+
+// Create a user
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const newUser = await User.create(req.body);
+        const token = jwt.sign(
+            {
+                username: newUser.username,
+                userId: newUser.id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "2h"
+            }
+        );
+        res.json({
+            token,
+            user: newUser
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            msg: "error creating user",
+            err
+        });
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        console.log(req.body);
+        const userData = await User.findOne({ where: { username: req.body.username } });
+        if (!userData) {
+            console.log("USERNAME NOT FOUND");
+            return res.status(403).json({ msg: "invalid login" });
+        } else if (!bcrypt.compareSync(req.body.password, userData.password)) {
+            return res.status(401).json({ msg: "invalid login" });
+        } else {
+            const token = jwt.sign(
+                {
+                    username: userData.username,
+                    userId: userData.id
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h"
+                }
+            );
+            res.json({
+                token,
+                user: userData
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            msg: "error logging in",
+            err
+        });
+    }
+});
+
+// Verify Token
+router.get("/auth/verifytoken", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+        const foundUser = await User.findByPk(data.userId, {
+            include: [
+                {
+                    model: User,
+                    as: 'followers',
+                    attributes: { exclude: ['password', 'bio'] }
+                },
+                {
+                    model: User,
+                    as: 'following',
+                    attributes: { exclude: ['password', 'bio'] }
+                }
+            ]
+        });
+        res.json(foundUser);
+    } catch (err) {
+        console.log(err);
+        res.status(403).json({ msg: "bad token", err });
     }
 });
 
 // Update a user
-router.put('/:id', (req, res) => {
-    const userId = req.params.id;
-    const userData = req.body;
+router.put('/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const userData = req.body;
 
-    const user = User.findByPk(userId)
-    if(!user) {
-        return res.status(404).json({msg: "user not found"});
-    };
-    User.update(userData, {where:{id:userId}})
-    .then(users => {
-        res.json(users)
-    }).catch(err => {
-        console.log(err)
-        res.status(500).json({
-            msg:"error", err
-        });
-    });
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ msg: "user not found" });
+        }
+
+        await User.update(userData, { where: { id: userId } });
+
+        res.json({ msg: "User updated successfully" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "error", err });
+    }
 });
 
 // Add a friend
@@ -263,21 +265,21 @@ router.put('/addfriend/:id', async (req, res) => {
 // });
 
 // Delete a user
-router.delete('/id', (req, res) => {
-    User.destroy({
-        where: {id: req.params.id}
-    })
-    .then(
+router.delete('/id', async (req, res) => {
+    try {
+        await User.destroy({
+            where: {id: req.params.id}
+        });
         res.status(200).json({
             msg: "Deleted"
-        })
-    ).catch(err => {
-        console.log(err)
+        });
+    } catch (err) {
+        console.log(err);
         res.status(500).json({
-            msg:"error deleting user",
+            msg: "error deleting user",
             err
         });
-    });
+    }
 });
 
 module.exports = router;
